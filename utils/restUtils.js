8aa1075f030c62f8da1bdb9ee1b6de7ba971bf36
Jon Eubank <joneubank@gmail.com>
2019-08-30 13:41:20 -0400

    Clinical Registration Endpoints (#7)
    * :sparkles: Clinical Submission system registration services
    
    * :sparkles: Clinical Service schema for registration
    
    * :bug: Add missing config value for clinical url
    
    * :bug: Correct data conversion from registration q's
    
    * :sparkles: Include Clinical schema in Apps
    
    * :sparkles: Fix error handling for clear registration data
    
    * :sparkles: Registration commit returns new sample ids
    
    * :bulb: Clarifying Response value in schema
    
    * :sparkles: Validate JWT has Write scope before file upload
    
    * :sparkles: Send filename to clinical service
    
    * :sparkles: Dynamically resolve submission fields
    
    * :sparkles: Introduce data for future added registration fields
    
    * :wrench: Added clinical root to env schema
    
    * :recycle: Move rest error handler to a rest utils helper
    
    * :bug: Fix rebase error, package version update
    
    * :bug: Fix naming issues for import
    
    * :pushpin: Package-lock update to match package
    
    * :pushpin: Correcting ProgramService proto after merge err
    
    * :ok_hand: Remove unused variable
    
    * :ok_hand: Simplify Authorization context creation
    
    * :bug: Remove typo line

---
+++ utils/restUtils.js
@@ -0,0 +1,27 @@
+import { AuthenticationError, UserInputError, ServerError } from 'apollo-server-express';
+/*
+convert the REST status codes to GQL errors, or return the response if passing
+*/
+export const restErrorResponseHandler = async response => {
+  switch (response.status) {
+    case 200:
+    case 201:
+      return response;
+    case 401:
+    case 403:
+      throw new AuthenticationError(response.status);
+    case 404:
+      let notFoundData;
+      try {
+        // This was built for the response structure from Clincial Service which returns a message value in the 404 response.
+        notFoundData = await response.json();
+      } catch {
+        notFoundData = { message: '' };
+      }
+      throw new UserInputError(notFoundData.message);
+    case 500:
+      throw new ServerError();
+    default:
+      return response;
+  }
+};
