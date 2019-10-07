75b76f24195667fce372d58a8d5262de7a82e3a6
jaserud <udjaser@gmail.com>
2019-10-07 17:26:59 -0400

    added support for all.zip

--- routes/clinical.js
+++ routes/clinical.js
@@ -1,7 +1,7 @@
 import urlJoin from 'url-join';
 import { CLINICAL_SERVICE_ROOT, SUBMISSION_TEMPLATE_PATH } from '../config';
 import fetch from 'node-fetch';
-
+import logger from '../utils/logger';
 var express = require('express');
 var router = express.Router();
 
@@ -10,14 +10,29 @@ var router = express.Router();
 // so that the user is automatically prompted to save the file from their browser.
 const apiRoot = urlJoin(CLINICAL_SERVICE_ROOT, SUBMISSION_TEMPLATE_PATH);
 
+// This is mostly passthrough, so file names and response should be set in clincial
 router.get('/template/:template', async (req, res) => {
-  const name = req.params.template.replace(/.tsv$/, '');
-  const data = fetch(urlJoin(apiRoot, name)).then(r => {
-    res.status(r.status);
-    res.headers = r.headers;
-    // copying headers doesn't copy the content-type header...
-    res.contentType(r.headers.get('content-type'));
-    res.send(r.body.read());
-  });
+  const name = req.params.template.replace(/.tsv$/, '').replace(/.zip$/, '');
+  fetch(urlJoin(apiRoot, name))
+    .then(r => {
+      res.status(r.status);
+      res.headers = r.headers.raw();
+      // copying headers doesn't copy the content-type&content-disposition header...
+      res.set({
+        'Content-Type': r.headers.get('content-type') || 'application/json',
+        'Content-Disposition': r.headers.get('content-disposition') || 'inline',
+      });
+      // pass buffered data to next '.then'
+      return r.buffer(); // buffer() turns the body(r) into the buffred data(file)
+    })
+    .then(bufferedData => res.send(bufferedData))
+    .catch(err => handleError(err, res));
 });
+
+// This is for handling errors not captured by clinical (e.g. connection failed)
+function handleError(err, res) {
+  logger.error('Clinical Router Error - ' + err);
+  return res.status(500).send('Internal Server Error');
+}
+
 module.exports = router;
