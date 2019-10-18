f706f4927ac920ae5693836c68ab342028bb0d42
jaserud <udjaser@gmail.com>
2019-10-18 15:48:51 -0400

    remove .zip extension for all and moar comments

--- routes/clinical.js
+++ routes/clinical.js
@@ -10,7 +10,9 @@ var router = express.Router();
 // so that the user is automatically prompted to save the file from their browser.
 const apiRoot = urlJoin(CLINICAL_SERVICE_ROOT, SUBMISSION_TEMPLATE_PATH);
 
-// This is mostly passthrough, so file names and response should be set in clincial
+// This is mostly passthrough, so file names and response will be set in clincial
+// 'all'.zip or 'all' will retrieve the zip file with all templates
+// for specific templates 'templateName'.tsv or 'templateName' will get the tsv from clinical
 router.get('/template/:template', async (req, res) => {
   const name = req.params.template.replace(/.tsv$/, '').replace(/.zip$/, '');
   fetch(urlJoin(apiRoot, name))
