d7331bd14eb668a9d70d517d4102af4021c4573c
Minh <hlminh2000@gmail.com>
2019-11-05 13:14:16 -0500

    🐛 keepalive_permit_without_calls must be number

--- services/programService/index.js
+++ services/programService/index.js
@@ -27,7 +27,7 @@ const proto = grpc.loadPackageDefinition(packageDefinition).program_service;
 const GRPC_CONFIG = {
   'grpc.keepalive_time_ms': 10000,
   'grpc.keepalive_timeout_ms': 5000,
-  'grpc.keepalive_permit_without_calls': 'true',
+  'grpc.keepalive_permit_without_calls': 1,
   'grpc.http2.max_pings_without_data': 0,
   'grpc.http2.min_time_between_pings_ms': 10000,
   'grpc.http2.min_ping_interval_without_data_ms': 5000,
