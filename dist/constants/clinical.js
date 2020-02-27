700b96fb371af5e70d31c861dd739e07d913b647
Minh <hlminh2000@gmail.com>
2020-02-27 15:01:56 -0500

    :fire: removes ./dist

--- dist/constants/clinical.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ERROR_CODES = {
-    INVALID_FILE_NAME: 'INVALID_FILE_NAME',
-};
-//# sourceMappingURL=clinical.js.map
