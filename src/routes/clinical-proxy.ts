95df0a804be86869497b166604b65a8870e9e69f
jaserud <udjaser@gmail.com>
2020-05-15 10:54:23 -0400

    code styling udpates

--- src/routes/clinical-proxy.ts
+++ src/routes/clinical-proxy.ts
@@ -11,38 +11,36 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 // that it returns be json, and we want to be able to return other content types, such as tab-separated-values,
 // so that the user is automatically prompted to save the file from their browser.
 
-// 'all' will retrieve the zip file with all templates
-// for specific templates 'templateName'.tsv or 'templateName' will get the tsv from clinical
-const templatePathRewrite = (pathName: string, req: Request) => {
-  const name = req.params.template.replace(/.tsv$/, '');
-  return urlJoin(SUBMISSION_TEMPLATE_PATH, name);
+// This is for handling nodejs/system errors (e.g. connection failed)
+const handleError = (err: Error, req: Request, res: Response) => {
+  logger.error('Clinical Router Error - ' + err);
+  return res.status(500).send('Internal Server Error');
 };
+
 router.use(
   '/template/:template',
   createProxyMiddleware({
     target: CLINICAL_SERVICE_ROOT,
-    pathRewrite: templatePathRewrite,
+    pathRewrite: (pathName: string, req: Request) => {
+      // 'all' will retrieve the zip file with all templates
+      // for specific templates 'templateName'.tsv or 'templateName' will get the tsv from clinical
+      const name = req.params.template.replace(/.tsv$/, '');
+      return urlJoin(SUBMISSION_TEMPLATE_PATH, name);
+    },
     onError: handleError,
   }),
 );
 
-const programTsvExportPathRewrite = (pathName: string, req: Request) => {
-  const programId = req.params.programId;
-  return urlJoin('/clinical/program/', programId, '/tsv-export');
-};
 router.use(
   '/program/:programId/all-clinical-data',
   createProxyMiddleware({
     target: CLINICAL_SERVICE_ROOT,
-    pathRewrite: programTsvExportPathRewrite,
+    pathRewrite: (pathName: string, req: Request) => {
+      const programId = req.params.programId;
+      return urlJoin('/clinical/program/', programId, '/tsv-export');
+    },
     onError: handleError,
   }),
 );
-
-// This is for handling nodejs/system errors (e.g. connection failed)
-function handleError(err: Error, req: Request, res: Response) {
-  logger.error('Clinical Router Error - ' + err);
-  return res.status(500).send('Internal Server Error');
-}
 
 export default router;
