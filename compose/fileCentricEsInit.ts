e50adba22cc14eb611d269b1cfd19b64b4ea5fe3
Minh <hlminh2000@gmail.com>
2020-05-15 15:33:51 -0400

    :bug: fixes indexing script

--- compose/fileCentricEsInit.ts
+++ compose/fileCentricEsInit.ts
@@ -3,29 +3,47 @@ import indexSettings from './file_centric/file_mapping.json';
 import indexData from './file_centric/sample_file_centric.json';
 
 (async () => {
+  const TEST_INDEX = 'file-centric';
+  const ELASTICSEARCH_HOST = 'https://localhost:9200';
   const esClient = new Client({
-    node: 'http://localhost:9200',
+    node: ELASTICSEARCH_HOST,
+    ssl: {
+      rejectUnauthorized: false,
+    },
   });
-  const TEST_INDEX = 'file_centric_test';
   try {
+    await esClient.ping();
+  } catch (err) {
+    console.log(`failing to ping elasticsearch at ${ELASTICSEARCH_HOST}: `, err);
+    throw err;
+  }
+  try {
+    console.log(`deleting index ${TEST_INDEX}`);
     await esClient.indices.delete({
       index: TEST_INDEX,
     });
   } catch (err) {
+    console.log(`could not delete index ${TEST_INDEX}: `, err);
     if ((await esClient.indices.exists({ index: TEST_INDEX })).body) {
       throw err;
     }
   }
+
+  console.log(`creating index ${TEST_INDEX}`);
 
   await esClient.indices.create({
     index: TEST_INDEX,
     body: indexSettings,
   });
 
+  console.log('index created');
+
   await Promise.all(
-    indexData.map(doc =>
-      esClient.index({
+    indexData.map((doc, index) => {
+      console.log(`doc_${index}`);
+      return esClient.index({
         index: TEST_INDEX,
+        refresh: 'wait_for',
         body: {
           ...doc,
           donors: [doc.donors].map(donor => ({
@@ -37,8 +55,9 @@ import indexData from './file_centric/sample_file_centric.json';
           })),
           repositories: [doc.repositories],
         },
-      }),
-    ),
+      });
+    }),
   );
+
   console.log('Complete!');
 })();
