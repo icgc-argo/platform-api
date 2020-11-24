699f295261e6734d48bbbc4787557feb57892901
Minh <hlminh2000@gmail.com>
2020-11-24 17:20:41 -0500

    :hankey: temporarily drops in some sqon types

---
+++ src/schemas/Arranger/types.ts
@@ -0,0 +1,47 @@
+/** @todo: use these types */
+
+export type ArrayFieldKeys = 'in' | 'is' | 'filter';
+
+export type ScalarFieldKeys = '>=' | '<=' | '>' | '<';
+
+export type CombinationKeys = 'and' | 'or' | 'not';
+
+export type ArrayFieldValue = Array<string | number> | string;
+export type ScalarFieldValue = number;
+
+export interface FilterField {
+  fields: string[];
+  value: ArrayFieldValue;
+}
+
+export interface FilterFieldOperator {
+  op: ArrayFieldKeys;
+  content: FilterField;
+}
+
+export interface ArrayField {
+  field: string;
+  value: ArrayFieldValue;
+}
+
+export interface ScalarField {
+  field: string;
+  value: ScalarFieldValue;
+}
+
+export interface ArrayFieldOperator {
+  op: ArrayFieldKeys;
+  content: ArrayField;
+}
+
+export interface ScalarFieldOperator {
+  op: ScalarFieldKeys;
+  content: ScalarField;
+}
+
+export type FieldOperator = ArrayFieldOperator | ScalarFieldOperator;
+
+export type RecursiveFilter = {
+  op: CombinationKeys;
+  content: Array<RecursiveFilter | FieldOperator | FilterFieldOperator>;
+};
