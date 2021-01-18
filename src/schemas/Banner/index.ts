c7c4f2672e3fd8716e7b9b171727263877f924db
Sam Rich <samrichca@users.noreply.github.com>
2021-01-18 14:53:02 -0500

    add error handling

--- src/schemas/Banner/index.ts
+++ src/schemas/Banner/index.ts
@@ -1,8 +1,21 @@
 import { gql } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
 
-const bannersStr = process.env.BANNERS || '';
-const bannerArray = JSON.parse(bannersStr);
+const getBanners = () => {
+  try {
+    const bannersStr = process.env.BANNERS || '';
+    const banners = JSON.parse(bannersStr);
+    if (!Array.isArray(banners)) {
+      throw new Error('Banners need to be an array');
+    } 
+    return banners;
+  } catch (e) {
+    console.log('Banners error: ', e.name, e.message)
+    return [];
+  }
+}
+
+const bannersArray = getBanners();
 
 const typeDefs = gql`
   type Banner {
@@ -19,7 +32,7 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    banners: () => bannerArray,
+    banners: () => bannersArray,
   }
 };
 
