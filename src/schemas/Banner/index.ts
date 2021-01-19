28dc2f728c70b757deb3348dff92bd7220007f50
Sam Rich <samrichca@users.noreply.github.com>
2021-01-19 17:00:22 -0500

    wip using library

--- src/schemas/Banner/index.ts
+++ src/schemas/Banner/index.ts
@@ -1,7 +1,10 @@
 import { gql } from 'apollo-server-express';
 import { makeExecutableSchema } from 'graphql-tools';
-import { isEqual } from 'lodash';
-import { validateType } from "ts-validate-type";
+import bannerTI from "./banner-ti";
+import { createCheckers } from "ts-interface-checker";
+import logger from '../../utils/logger';
+
+const { BannerTI } = createCheckers(bannerTI);
 
 interface Banner {
   dismissable: boolean;
@@ -11,96 +14,45 @@ interface Banner {
   title: string;
 }
 
-function instanceOfBanner(banner: Banner): banner is Banner {
-  console.log('title' in banner);
-  return 'title' in banner;
-}
-
-// type BannerObj = {
-//   [key: string]: string
-// }
-
-// const bannerFieldsRequired: BannerObj = {
-//   'dismissable': 'boolean',
-//   'id': 'number',
-//   'level': 'string',
-//   'title': 'string',
-// };
-
-// const bannerFieldsAllowed: BannerObj = {
-//   ...bannerFieldsRequired,
-//   'message': 'string',
-// };
-
-// const bannerLevelsAllowed = [
-//   'error',
-//   'info',
-//   'warning',
-// ];
-
-// const getBanners = () => {
-//   try {
-//     const bannersStr = process.env.BANNERS || '';
-//     const bannersParsed = JSON.parse(bannersStr);
-
-//     if (!Array.isArray(bannersParsed)) {
-//       throw new Error('Banners need to be an array');
-//     }
-
-//     bannersParsed.forEach(banner => {
-//       const errorTitle = `"${banner.title}"`;
-//       const bannerKeys = Object.keys(banner).sort();
-
-//       const checkFields = isEqual(bannerKeys, Object.keys(bannerFieldsRequired).sort()) || isEqual(bannerKeys, Object.keys(bannerFieldsAllowed).sort());
-
-//       if (!checkFields) {
-//         throw new Error(`Fields incorrect in ${errorTitle}`);
-//       }
-
-//       for (const [key, value] of Object.entries(banner)) {
-//         const expectedType = bannerFieldsAllowed[key];
-//         const checkType = expectedType === typeof value;
-//         if (!checkType) {
-//           throw new Error(`${key} should be a ${expectedType} in ${errorTitle}`);
-//         }
-//       }
-      
-//       const checkLevel = bannerLevelsAllowed.includes(banner.level);
-//       if (!checkLevel) {
-//         throw new Error(`Invalid level "${banner.level}" in ${errorTitle}`);
-//       }
-//     });
-
-//     return bannersParsed;
-//   } catch (e) {
-//     console.log('Banners', e.name, '-', e.message);
-//     return [];
-//   }
-// };
+const logError = (message: string, name: string) => {
+  logger.error(`Banner - ${name} - ${message}`);
+};
 
 const getBanners = () => {
+  let result: Banner[] = [];
+
   try {
     const bannersStr = process.env.BANNERS || '';
     const bannersParsed = JSON.parse(bannersStr);
 
     if (!Array.isArray(bannersParsed)) {
-      // TODO: check if it's an instance of banner
-      throw 'Banners need to be an array';
+      const isBanner = !BannerTI.test(bannersParsed);
+      if (isBanner) {
+        result = [bannersParsed];
+      } else {
+        throw 'Banners need to be an array';
+      }
     }
 
-    const bannersArray = bannersParsed.filter(banner => {
-      // TODO: check if it's an instance of banner
-      return true;
+    result = bannersParsed.filter((banner: any) => {
+      const isBanner = BannerTI.test(banner);
+      console.log(isBanner)
+      return isBanner;
     });
 
-    return bannersParsed;
+    bannersParsed.forEach((banner: any) => {
+      BannerTI.check(banner);
+    });
   } catch (e) {
-    console.log(e.name, e.message)
-    return [];
+    logError(e.message, e.name);
+  } finally {
+    return result;
   }
 }
 
 const bannersArray = getBanners();
+
+console.log({ bannersArray });
 
 const typeDefs = gql`
   type Banner {
