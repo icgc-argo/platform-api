76d49b872b7ecdbe24ce7ca0048f4b0804de5fc0
Sam Rich <samrichca@users.noreply.github.com>
2021-01-20 15:13:21 -0500

    move allowed fields to type & const

--- src/schemas/Banner/index.ts
+++ src/schemas/Banner/index.ts
@@ -6,6 +6,14 @@ import logger from '../../utils/logger';
 
 const { BannerTI } = createCheckers(bannerTI);
 
+type AllowedFields =
+  | 'dismissable' 
+  | 'id' 
+  | 'level'
+  | 'message'
+  | 'title'
+;
+
 interface Banner {
   dismissable: boolean;
   id: number;
@@ -13,6 +21,14 @@ interface Banner {
   message?: string;
   title: string;
 }
+
+const allowedFields = [
+  'dismissable',
+  'id',
+  'level',
+  'message',
+  'title',
+];
 
 const logError = (message: string, name: string) => {
   logger.error(`📜 Banner - ${name} - ${message}`);
@@ -23,6 +39,7 @@ const getBanners = () => {
 
   try {
     const bannersStr = process.env.BANNERS || '';
+
     const bannersParsed = [].concat(JSON.parse(bannersStr));
 
     const bannersValidated = bannersParsed.reduce((acc, curr) => ({
@@ -31,12 +48,16 @@ const getBanners = () => {
         ? { valid: [...acc.valid, curr] }
         : { invalid: [...acc.invalid, curr] }
       )
-    }), { valid: [], invalid: [] })
+    }), { valid: [], invalid: [] });
 
-    result = bannersValidated.valid.map((banner: Banner) => {
-      const { dismissable, id, level, title, message = '' } = banner;
-      return ({ dismissable, id, level, title, message });
-    });
+    const bannersOnlyAllowedFields = bannersValidated.valid.map((banner: Banner) => 
+      Object.keys(banner)
+        .filter(key => allowedFields.includes(key))
+        .reduce((acc, curr: AllowedFields) => 
+          ({ ...acc, [curr]: banner[curr] }), {} as Banner)
+    );
+
+    result = bannersOnlyAllowedFields;
 
     bannersValidated.invalid.forEach((banner: any) => {
       // check() will throw errors on invalid banners
