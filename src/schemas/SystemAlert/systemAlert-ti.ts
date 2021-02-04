472b3ccd97b1bd0e8556e8acfc56331f874619fa
Sam Rich <samrichca@users.noreply.github.com>
2021-02-04 10:01:44 -0500

    rc/3.13.2 (#369)

---
+++ src/schemas/SystemAlert/systemAlert-ti.ts
@@ -0,0 +1,16 @@
+import * as t from 'ts-interface-checker';
+
+// this was created manually, not with ts-interface-builder
+
+export const SystemAlertTI = t.iface([], {
+  dismissable: 'boolean',
+  id: 'string',
+  level: t.union(t.lit('error'), t.lit('info'), t.lit('warning')),
+  message: t.opt('string'),
+  title: 'string',
+});
+
+const exportedTypeSuite: t.ITypeSuite = {
+  SystemAlertTI,
+};
+export default exportedTypeSuite;
