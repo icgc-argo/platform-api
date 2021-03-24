15653685e88024c7ba02a69f9f8ae83d12b9b9c9
Sam Rich <samrichca@users.noreply.github.com>
2021-03-24 14:35:15 -0400

    improve gql type defs

--- src/schemas/ProgramDonorPublishedAnalysisByDateRange/gqlTypeDefs.ts
+++ src/schemas/ProgramDonorPublishedAnalysisByDateRange/gqlTypeDefs.ts
@@ -22,271 +22,35 @@ import { gql } from 'apollo-server-express';
 export default gql`
   scalar DateTime
 
-  enum DonorMolecularDataReleaseStatus {
-    FULLY_RELEASED
-    PARTIALLY_RELEASED
-    NO_RELEASE
+  enum AnalysisTitle {
+    alignment
+    mutect
+    rawReads
+    sangerVcs
   }
-
-  enum DonorMolecularDataProcessingStatus {
-    COMPLETE
-    PROCESSING
-    REGISTERED
-  }
-
-  enum ProgramDonorSummaryEntryField {
-    donorId
-    validWithCurrentDictionary
-    releaseStatus
-    submitterDonorId
-    programShortName
-    submittedCoreDataPercent
-    submittedExtendedDataPercent
-    registeredNormalSamples
-    registeredTumourSamples
-    publishedNormalAnalysis
-    publishedTumourAnalysis
-    alignmentsCompleted
-    alignmentsRunning
-    alignmentsFailed
-    sangerVcsCompleted
-    sangerVcsRunning
-    sangerVcsFailed
-    mutectCompleted
-    mutectRunning
-    mutectFailed
-    processingStatus
-    updatedAt
-    createdAt
-    alignmentFirstPublishedDate
-    fakeFirstPublishedDate
-    mutectFirstPublishedDate
-    rawReadsFirstPublishedDate
-    sangerVcsFirstPublishedDate
-  }
-
-  input ProgramDonorSummaryFilter {
-    field: ProgramDonorSummaryEntryField!
-    values: [String!]!
-  }
-
-  enum SortOrder {
-    asc
-    desc
-  }
-
-  input DonorSummaryEntrySort {
-    field: ProgramDonorSummaryEntryField!
-    order: SortOrder
-  }
-
-  """
-  Includes status summary of clinical and molecular data processing for the given donor
-  """
-  type DonorSummaryEntry {
-    """
-    Unique object ID for this summary object
-    """
-    id: ID!
-    """
-    Donor id of the donor within the program
-    """
-    donorId: String!
-    """
-    Short name of the program in which this donor is registered
-    """
-    programShortName: String!
-    """
-    Whether the donor submitted donor is valid according to the latest data dictionary layed out at: https://docs.icgc-argo.org/dictionary
-    """
-    validWithCurrentDictionary: Boolean!
-    """
-    Release status of the donor's molecular data
-    """
-    releaseStatus: DonorMolecularDataReleaseStatus!
-    """
-
-    """
-    submitterDonorId: String!
-    """
-    Percentage of core clinical data fields that has been submitted for this donor. All core fields are listed at: https://docs.icgc-argo.org/dictionary
-    """
-    submittedCoreDataPercent: Float!
-    """
-    Percentage of extended clinical data fields that has been submitted for this donor. All extended fields are listed at: https://docs.icgc-argo.org/dictionary
-    """
-    submittedExtendedDataPercent: Float!
-    """
-    Number of normal samples registered for this donor
-    """
-    registeredNormalSamples: Int!
-    """
-    Number of tumour samples registered for this donor
-    """
-    registeredTumourSamples: Int!
-    """
-    Number of normal sample analysis that has been published for this donor
-    """
-    publishedNormalAnalysis: Int!
-    """
-    Number of tumour sample analysis that has been published for this donor
-    """
-    publishedTumourAnalysis: Int!
-    """
-    Number of alignments completed for this donor
-    """
-    alignmentsCompleted: Int!
-    """
-    Number of alignments currently running for this donor
-    """
-    alignmentsRunning: Int!
-    """
-    Number of alignments that is failing for this donor
-    """
-    alignmentsFailed: Int!
-    """
-    Number of Sanger VCs completed for this donor
-    """
-    sangerVcsCompleted: Int!
-    """
-    Number of Sanger VCs currently running for this donor
-    """
-    sangerVcsRunning: Int!
-    """
-    Number of Sanger VCs that is failing for this donor
-    """
-    sangerVcsFailed: Int!
-    """
-    Number of Mutect2 completed for this donor
-    """
-    mutectCompleted: Int!
-    """
-    Number of Mutect2 currently running for this donor
-    """
-    mutectRunning: Int!
-    """
-    Number of Mutect2 that is failed for this donor
-    """
-    mutectFailed: Int!
-    """
-    Molecular data processing status of this donor
-    """
-    processingStatus: DonorMolecularDataProcessingStatus!
-    """
-    Timestamp of the latest update applied to this donor's clinical data
-    """
-    updatedAt: DateTime!
-    """
-    Timestamp of when this donor was first registered
-    """
-    createdAt: DateTime!
-    """
-    Timestamp of when raw reads data was first published
-    """
-    rawReadsFirstPublishedDate: DateTime!
-    """
-    This one is fake. I want to see if unused fields crash the app.
-    """
-    fakeFirstPublishedDate: DateTime!
-    """
-    Timestamp of when mutect data was first published
-    """
-    mutectFirstPublishedDate: DateTime!
-    """
-    Timestamp of when alignment data was first published
-    """
-    alignmentFirstPublishedDate: DateTime!
-    """
-    Timestamp of when Sanger VCS data was first published
-    """
-    sangerVcsFirstPublishedDate: DateTime!
-  }
-
-  """
-  Contains summary of aggregate clinical and molecular data processing status for the given program
-  """
-  type ProgramDonorSummaryStats {
-    """
-    Unique ID of this summary object
-    """
-    id: ID!
-    """
-    Short name of the program which this summary object is associated with
-    """
-    programShortName: String!
-    """
-    Total number of donors registered for this program
-    """
-    registeredDonorsCount: Int!
-    """
-    Percentage of core clinical data fields submitted over total core clinical data fields
-    """
-    percentageCoreClinical: Float!
-    """
-
-    """
-    percentageTumourAndNormal: Float!
-    """
-    Number of donors whose molecular data is being processed
-    """
-    donorsProcessingMolecularDataCount: Int!
-    """
-    Number of files to QC
-    """
-    filesToQcCount: Int!
-    """
-    Number of donors whose files have been released
-    """
-    donorsWithReleasedFilesCount: Int!
-    """
-    Number of donors invalidated with current data dictionary version
-    """
-    donorsInvalidWithCurrentDictionaryCount: Int!
-    """
-    Total number of genomic files associated with this program
-    """
-    allFilesCount: Int!
-    """
-    Number of donors whose genomic files have been fully released
-    """
-    fullyReleasedDonorsCount: Int!
-    """
-    Number of donors who only have some genomic files that have been released
-    """
-    partiallyReleasedDonorsCount: Int!
-    """
-    Number of donors registered to the program who currently has no released genomic file
-    """
-    noReleaseDonorsCount: Int!
-  }
-
-  # new types for donor data
 
   enum AnalysisType {
     clinical
     molecular
   }
 
-  type ResultBucketBuckets {
-    date: String
+  type DateRangeBucket {
+    date: DateTime
     donors: Int
   }
 
-  type ResultBucket {
-    title: String
-    buckets: [ResultBucketBuckets]
+  type AnalysisBucket {
+    title: AnalysisTitle
+    buckets: [DateRangeBucket]
   }
 
   type Query {
-    """
-    Paginated list of donor data summary given a program
-    """
     programDonorPublishedAnalysisByDateRange(
       analysisType: AnalysisType!
       dateRangeFrom: DateTime!
       dateRangeTo: DateTime!
       programShortName: String!
       bucketCount: Int = 7
-    ): [ResultBucket]!
+    ): [AnalysisBucket]!
   }
 `;
