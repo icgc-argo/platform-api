f1a686e2b53a6d9aef7f8fb756bb2e4ee0d2ba77
Jon Eubank <joneubank@gmail.com>
2021-03-31 17:27:31 -0400

    Apply topic permission validation to kafka endpoint using updated token-utils

--- src/routes/kafka-rest-proxy.ts
+++ src/routes/kafka-rest-proxy.ts
@@ -22,61 +22,73 @@ import urlJoin from 'url-join';
 import logger from '../utils/logger';
 import fetch from 'node-fetch';
 import { json } from 'body-parser';
-import express from 'express';
+import express, { Router } from 'express';
+import { EgoClient } from 'services/ego';
+import authenticatedRequestMiddleware, {
+  AuthenticatedRequest,
+} from 'routes/middleware/authenticatedRequestMiddleware';
 import egoTokenUtils from 'utils/egoTokenUtils';
 
-var router = express.Router();
-const apiRoot = KAFKA_REST_PROXY_ROOT;
+const createKafkaRouter = (egoClient: EgoClient): Router => {
+  const router = express.Router();
+  const apiRoot = KAFKA_REST_PROXY_ROOT;
 
-// fetch needs to use the json body parser
-router.use(json());
+  // fetch needs to use the json body parser
+  router.use(json());
 
-// middleware to secure the kafka proxy endpoint
-// it expects a valid jwt
-router.use((req, res, next) => {
-  const jwt = (req.headers.authorization || '').split(' ')[1] || '';
-  if (jwt === '') {
-    return res.status(401).send({
-      message: 'this endpoint needs a valid jwt token',
+  router.use(authenticatedRequestMiddleware({ egoClient }));
+
+  router.post('/:topic', async (req: AuthenticatedRequest, res) => {
+    const {
+      auth: { authenticated, serializedScopes, egoJwt },
+      params: { topic },
+    } = req;
+
+    // Require auth
+    if (!authenticated) {
+      res.status(401).json({ error: 'invalid auth token' });
+      return;
+    }
+
+    // Require kafka topic writing permisison
+    const hasPermission = egoTokenUtils.canWriteKafkaTopic({
+      permissions: serializedScopes,
+      topic,
     });
-  }
+    if (!hasPermission) {
+      res.status(403).json({ error: 'not authorized' });
+    }
 
-  if (!egoTokenUtils.isValidJwt(jwt)) {
-    return res.status(401).send({
-      message: 'expired token',
+    const url = urlJoin(apiRoot, 'topics', topic);
+    const msg = req.body;
+    logger.debug(`received message in kafka proxy ${JSON.stringify(msg)}`);
+    const kafkaRestProxyBody = JSON.stringify({
+      records: [
+        {
+          value: msg,
+        },
+      ],
     });
-  }
-  return next();
-});
-
-router.post('/:topic', async (req, res) => {
-  const url = urlJoin(apiRoot, 'topics', req.params.topic);
-  const msg = req.body;
-  logger.debug(`received message in kafka proxy ${JSON.stringify(msg)}`);
-  const kafkaRestProxyBody = JSON.stringify({
-    records: [
-      {
-        value: msg,
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/vnd.kafka.json.v2+json',
+        Accept: 'application/vnd.kafka.v2+json',
       },
-    ],
-  });
-  return fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/vnd.kafka.json.v2+json',
-      Accept: 'application/vnd.kafka.v2+json',
-    },
-    body: kafkaRestProxyBody,
-  })
-    .then(response => {
-      res.contentType('application/vnd.kafka.v2+json');
-      res.status(response.status);
-      return response.body.pipe(res);
+      body: kafkaRestProxyBody,
     })
-    .catch(e => {
-      logger.error('failed to send message to kafka proxy' + e);
-      return res.status(500).send(e);
-    });
-});
+      .then(response => {
+        res.contentType('application/vnd.kafka.v2+json');
+        res.status(response.status);
+        return response.body.pipe(res);
+      })
+      .catch(e => {
+        logger.error('failed to send message to kafka proxy' + e);
+        return res.status(500).send(e);
+      });
+  });
 
-export default router;
+  return router;
+};
+
+export default createKafkaRouter;
