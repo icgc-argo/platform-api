2e4747960ee84a06c45a62f0682d00b3e6c226f5
Dan <ddemaria@oicr.on.ca>
2022-06-16 12:41:03 -0400

    Hotfix: Restore Clinical Download Endpoint (#569)
    * Restore Original Endpoint
    
    * Make ClinicalErrors not Null

--- src/schemas/Clinical/gqlTypeDefs.ts
+++ src/schemas/Clinical/gqlTypeDefs.ts
@@ -187,7 +187,7 @@ export default gql`
     programShortName: String!
     clinicalEntities: [ClinicalDataEntities]!
     completionStats: [CompletionStats]
-    clinicalErrors(filters: ClinicalInput): [ClinicalErrors]
+    clinicalErrors(filters: ClinicalInput): [ClinicalErrors]!
   }
 
   """
